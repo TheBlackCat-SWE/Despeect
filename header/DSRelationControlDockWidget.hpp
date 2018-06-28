11b926ab5b16c4f87f889a2e65c00839294d6604
sscaglio <blackpaper1993@gmail.com>
2018-06-28 16:11:44 +0200

    definizione file iniziali dock feature

---
+++ header/DSRelationControlDockWidget.hpp
@@ -0,0 +1,12 @@
+#ifndef DSRELATIONCONTROLDOCKWIDGET_H
+#define DSRELATIONCONTROLDOCKWIDGET_H
+
+#include <QObject>
+
+class DSRelationControlDockWidget : public QDockWidget
+{
+public:
+    DSRelationControlDockWidget();
+};
+
+#endif // DSRELATIONCONTROLDOCKWIDGET_H
