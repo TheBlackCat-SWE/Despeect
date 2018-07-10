51bc31e28fb97e9c7b8689d1fca47630799f9767
davidedisomma1 <davidedi.somma@hotmail.it>
2018-07-10 10:40:16 +0200

    aggiunti test

--- Test/simple_test.cpp
+++ Test/simple_test.cpp
@@ -10,26 +10,45 @@ using std::string;
 
 TEST(DSAdapter, FailedVoiceLoad) {
     DSAdapter* a=DSAdapter::createAdapter();
-    string voice_path("prova.json");
+    string voice_path("test.json");
     a->loadVoice(voice_path);
-    a->loadText("Sono un test");
-    EXPECT_EQ(a->execUttType("prova"),0);
+    a->loadText("I'm a test");
+    EXPECT_EQ(a->execUttType("test"),0);
 }
+
+
 
 TEST(DSAdapter, CorrectVoiceLoad) {
     DSAdapter* b=DSAdapter::createAdapter();
     string voice_path("../../Despeect/SpeectLib/voices/cmu_arctic_slt/voice.json");
     b->loadVoice(voice_path);
-    b->loadText("Sono un test");
+    b->loadText("I'm a test");
     EXPECT_EQ(b->execUttType("text"),1);
 }
 
+TEST(DSAdapter, CorrectTextLoad) {
+    DSAdapter* b=DSAdapter::createAdapter();
+    string voice_path("../../Despeect/SpeectLib/voices/cmu_arctic_slt/voice.json");
+    b->loadVoice(voice_path);
+    b->loadText("I'm a test");
+    EXPECT_EQ(b->getText(),"I'm a test");
+}
+
+TEST(DSAdapter, WrongRelationName) {
+    DSAdapter* b=DSAdapter::createAdapter();
+    string voice_path("../../Despeect/SpeectLib/voices/cmu_arctic_slt/voice.json");
+    b->loadVoice(voice_path);
+    b->loadText("I'm a test");
+    b->execUttType("text");
+    b->getRel("I'm a relation");
+    EXPECT_EQ(b->hasError(),0);
+}
 
 TEST(DSAdapter, CorrectRelationName) {
     DSAdapter* b=DSAdapter::createAdapter();
     string voice_path("../../Despeect/SpeectLib/voices/cmu_arctic_slt/voice.json");
     b->loadVoice(voice_path);
-    b->loadText("Sono un test");
+    b->loadText("I'm a test");
     b->execUttType("text");
     EXPECT_EQ(b->getRel("Token")->getName(),"Token");
 }
